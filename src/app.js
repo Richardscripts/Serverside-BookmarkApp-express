8be31eb0d10d4936c2f5069da4152377783347b0
unknown <richardcarmenscott@gmail.com>
2020-09-20 10:10:06 -0400

    Initial

---
+++ src/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validateBearerToken');
+const errorHandler = require('./errorHandler');
+const bookmarkRouter = require('./bookmark-router/bookmark-router');
+
+const app = express();
+
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
+app.use(validateBearerToken);
+
+app.use('/bookmarks', bookmarkRouter);
+
+app.use(errorHandler);
+
+module.exports = app;
